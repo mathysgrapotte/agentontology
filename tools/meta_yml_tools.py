8c204516e574492b951df303df053dc320f387b5
Júlia Mir Pedrol <mirp.julia@gmail.com>
2025-06-05 12:46:08 +0200

    add  meta.yml tools

---
+++ tools/meta_yml_tools.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import yaml
+
+def get_meta_yml_file(module_name: str) -> dict:
+    """
+    Access the nf-core/modules repository and return the meta.yml file of the given module.
+    
+    Args:
+        module_name (str): The name of the module to get the meta.yml file for. 
+                            The module_name must be provided in the format <tool>_<subtool> or <tool>/<subtool> or <tool> <subtool>.
+                            The subtool is optional.
+                            For example, "bwa_align" or "fastqc" or "bwa align" or "bwa/align".
+    Returns:
+        dict: The meta.yml file of the given module in json/yaml format as a dictionary.
+    """
+    if "_" in module_name:
+        tool, subtool = module_name.split("_")
+    elif "/" in module_name:
+        tool, subtool = module_name.split("/")
+    elif " " in module_name:
+        tool, subtool = module_name.split(" ")
+    else:
+        tool, subtool = module_name, ""
+
+    if subtool:
+        url = f"https://raw.githubusercontent.com/nf-core/modules/refs/heads/master/modules/nf-core/{tool}/{subtool}/meta.yml"
+    else:
+        url = f"https://raw.githubusercontent.com/nf-core/modules/refs/heads/master/modules/nf-core/{tool}/meta.yml"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        return yaml.safe_load(response.text)
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"An error occurred while connecting to the URL: {url}. Error message: {e}")
+
+def extract_tools_from_meta_json(meta_file: dict) -> list:
+    """
+    Extract the tools from the meta.yml file.
+
+    Args:
+        meta_file (str): The content of the module meta.yml file in json format.
+
+    Returns:
+        list: A list of tools.
+    """
+    tools_names = []
+    tools_list = meta_file.get("tools", [])
+    for tool in tools_list:
+        tools_names.append(list(tool.keys())[0])
+    return tools_names
+
+def extract_information_from_meta_json(meta_file: dict, tool_name: str) -> dict:
+    """
+    Extract information metadata from an nf-core module meta.yml file.
+    Information extracted:
+        - inputs
+        - outputs
+        - homepage URL
+        - documentation URL
+        - bio.tools ID
+        
+
+    Args:
+        meta_file (str): The content of the module meta.yml file in json format.
+        tool_name (str): The name of the tool to extract information for.
+
+    Returns:
+        dict: A dictionary with the extracted metadata.
+            Each file or term can also contain additional metadata like 'description' or 'type'.
+
+    Example output:
+        {
+            "inputs": [...],
+            "outputs": [...],
+            "homepage": "https://www.bioinformatics.babraham.ac.uk/projects/fastqc/",
+            "documentation": "https://www.bioinformatics.babraham.ac.uk/projects/fastqc/Help/",
+            "bio_tools_id": "biotools:fastqc"
+        }
+    """
+    inputs = meta_file.get("input", [])
+    outputs = meta_file.get("output", [])
+    for tool in meta_file.get("tools", []):
+        if list(tool.keys())[0] == tool_name:
+            homepage_url = tool.get("homepage", "")
+            documentation_rul = tool.get("documentation", "")
+            bio_tools_id = tool.get("identifier", "")
+        print("Extracted metadata information from nf-core module meta.yml")
+    return {"inputs": inputs, "outputs": outputs, "homepage": homepage_url, "documentation": documentation_rul, "bio_tools_id": bio_tools_id}
