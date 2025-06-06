6d26f713d109d0027a4c34fcaca14c05bbe5a244
Suzanne Jin <suzanne.jin.yao@gmail.com>
2025-06-06 11:29:47 +0200

    add mathys agent

--- agents/query_ontology_db.py
+++ agents/query_ontology_db.py
@@ -2,8 +2,8 @@ from smolagents import CodeAgent, LiteLLMModel
 from tools.fetch_ontology_tools import search_edam_ontology_by_search_term, get_edam_description_from_ontology_format_class
 
 model = LiteLLMModel(
-    model_id="ollama/devstral:latest",
-    #model_id="ollama/qwen3:0.6b",
+    # model_id="ollama/devstral:latest",
+    model_id="ollama/qwen3:0.6b",
     api_base="http://localhost:11434",
     temperature=0.0,
     max_tokens=5000,
