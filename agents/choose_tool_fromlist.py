6a78615c5736112709b6f77c14faf861be12efe9
caraiz2001 <c.araiz2001@gmail.com>
2025-06-06 14:23:46 +0200

    add first version agent choose tool fromlist

--- agents/choose_tool_fromlist.py
+++ agents/choose_tool_fromlist.py
@@ -3,7 +3,7 @@ from tools.bio_tools_tools import get_biotools_response, get_biotools_ontology
 
 model = LiteLLMModel(
     # model_id="ollama/devstral:latest",
-    model_id="ollama/qwen3:0.6b",
+    model_id="ollama/devstral:latest",
     api_base="http://localhost:11434",
     temperature=0.0,
     max_tokens=5000,
